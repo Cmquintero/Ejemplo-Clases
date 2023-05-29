df7c192a00bcf0a824cdce1930ac444076348245
cmquintero <cmquinterot@ufpso.edu.co>
2023-05-29 11:33:33 -0500

    practicas de js y css

--- js/style.js
+++ js/style.js
@@ -1,1 +1,23 @@
-alert("Bienvenido a mi pagina ")
+console.log("Bienvenido a mi página");
+
+const enlacesHeader = document.querySelector(".enlaces-header");
+const sorieketon = document.querySelector(".sorieketon");
+let icoSorieketon = true;
+
+function alternarSorieketon() {
+  if (icoSorieketon) {
+    sorieketon.style.color = "#fff";
+  } else {
+    sorieketon.style.color = "#000";
+  }
+  icoSorieketon = !icoSorieketon;
+
+  enlacesHeader.classList.toggle("sorieketonopen");
+}
+
+sorieketon.addEventListener("click", alternarSorieketon);
+
+enlacesHeader.addEventListener("click", function() {
+  enlacesHeader.classList.toggle("sorieketonopen");
+  sorieketon.style.color = "#000";
+});
